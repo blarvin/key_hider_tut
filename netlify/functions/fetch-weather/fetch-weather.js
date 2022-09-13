97241723f7685a0b05134abf410b75b26dca0bcb
blarvin <blarvinius@gmail.com>
2022-09-13 12:51:58 +0100

    it was working on my machine -O-

---
+++ netlify/functions/fetch-weather/fetch-weather.js
@@ -0,0 +1,29 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const axios = require ('axios');
+
+const handler = async (event) => {
+const {lat, lon} = event.queryStringParameters
+
+  const API_SECRET = process.env.API_SECRET
+
+  const url = `http://api.weatherstack.com/current?access_key=${API_SECRET}&query=${lat},${lon}&forecast_days=4`
+
+  try {
+    const { data } = await axios.get(url)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+  }
+
+  catch (error) {
+    const {status, statusText, headers, data} = error.response
+    return {
+      statusCode: status,
+      body: JSON.stringify({status, statusText, headers, data})
+    }
+  }
+}
+
+module.exports = { handler }
